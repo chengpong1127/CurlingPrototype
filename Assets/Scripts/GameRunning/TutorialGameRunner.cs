0bac0c26cbe06c7a4cdc14747f58e99cf9abc6aa
Chengpong1127 <Chengpong1127@gmail.com>
2024-04-11 19:16:51 +0800

    Update GameScene with new tutorial elements

--- Assets/Scripts/GameRunning/TutorialGameRunner.cs
+++ Assets/Scripts/GameRunning/TutorialGameRunner.cs
@@ -1,7 +1,10 @@
+using UnityEngine;
+
 public class TutorialGameRunner : BaseGameRunner<TutorialGameRunner>
 {
+    [SerializeField] private GameObject m_tutorialElements;
     public override void StartGame()
     {
-        
+        m_tutorialElements.SetActive(true);
     }
 }
